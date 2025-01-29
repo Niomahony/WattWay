6a37846c8d39ce272aeb068a873207a2546ccef8
Niomahony <93268576+Niomahony@users.noreply.github.com>
2025-01-29 21:11:45 +0000

    Fixed issue with RNGestureHandler - package issue resolved by installing other random packages! Fun!

--- app.config.ts
+++ app.config.ts
@@ -37,7 +37,7 @@ export default {
       [
         "expo-splash-screen",
         {
-          image: "./assets/images/splash-icon.png",
+          image: "./assets/splash-icon.png",
           imageWidth: 200,
           resizeMode: "contain",
           backgroundColor: "#ffffff",
--- app/stack/App.tsx
+++ app/stack/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MapScreen from "../components/MapScreen";
 import NavigationScreen from "../components/NavigationScreen";
 import { RootStackParamList } from "../navigation/NavigationTypes";
